86a9eb90a6c1d3981705c44b8eb3120f62450449
dune <nathydune@gmail.com>
2024-12-27 22:04:28 +0100

    text home

--- src/components/Home/ByTheWay/ByTheWay.tsx
+++ src/components/Home/ByTheWay/ByTheWay.tsx
@@ -4,18 +4,16 @@ import Styles from './ByTheWay.module.scss';
 const ByTheWay: React.FC = () => {
     return (
         <div className={Styles.container}>
-                <h1>A propos de moi</h1>
-                <p>
-                    Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.<br /><br />
-                    Inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat imperiale praeceptum, dispicere litis exitialis certamina cogebatur abnuens et reclamans, adulatorum oblatrantibus turmis, bellicosus sane milesque semper et militum ductor sed forensibus iurgiis longe discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos ex isdem foveis cerneret emergentes, quae clam palamve agitabantur, occultis Constantium litteris edocebat inplorans subsidia, quorum metu tumor notissimus Caesaris exhalaret.<br /><br />
-                    Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis. Hic me meus in rem publicam animus pristinus ac perennis cum C. Caesare reducit, reconciliat, restituit in gratiam.<br /><br />
-                    Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis. Hic me meus in rem publicam animus pristinus ac perennis cum C. Caesare reducit, reconciliat, restituit in gratiam.<br /><br />
-                    Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.<br />
-                    Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.
-                </p>
-               
+            <h1>A propos de moi</h1>
+            <p>Après plus de 20 ans dans le milieu médical, j’ai décidé de donner un nouveau souffle à ma vie professionnelle. J’ai entrepris des cours à distance afin de passer un BEP EPCV (Études de Projets de Communication Visuelle).<br /><br />
+                Pendant cette formation, j’ai découvert des outils comme Photoshop, InDesign et Illustrator. Illustrator m’a particulièrement plu pour sa créativité et sa logique. J’ai même créé des cartes personnalisées pour mes enfants, dont vous pourrez découvrir des exemples dans la section suivante.<br /><br />
+                C’est en choisissant une option web pendant ce BEP que j’ai eu un véritable coup de cœur pour le code. J’ai commencé à apprendre les bases du HTML et du CSS via OpenClassrooms, et très vite, j’ai su que j’avais trouvé ma nouvelle voie.<br /><br />
+                Pour aller plus loin, j’ai suivi une formation intensive de développeuse web et mobile à la Wild Code School de Nantes. En 5 mois, j’ai appris à maîtriser JavaScript, React, et Node.js. Cela a été suivi d’un stage de 6 mois chez WIIO, à Saint-Sébastien-sur-Loire, qui s’est prolongé d’une année supplémentaire.<br /><br />
+                Chez WIIO, j’ai eu l’opportunité d’approfondir mes connaissances. J’ai appris à coder en TypeScript, j’ai également découvert le développement mobile avec React Native, et j’ai un peu exploré Kotlin Compose. J’ai également acquis de l’expérience en Windev et en Velocity, ce qui m’a permis d’élargir mon champ de compétences et de confirmer mon enthousiasme pour les technologies web et mobiles. Cette expérience m’a permis de m’impliquer dans des projets variés, où j’ai pu apprendre, expérimenter et grandir en tant que développeuse.<br /><br />
+                Aujourd’hui, je continue à apprendre et à évoluer dans ce métier qui me passionne, en mettant ma créativité, ma détermination et mon enthousiasme dans les projets sur lesquels je travaille (développement de sites, indexation sur Google…). Les projets que je vous présenterai sont des projets personnels ou des projets réalisés en groupe, au cours de ma formation à la Wild Code School.
+            </p>
         </div>
-            
+
     );
 }
 export default ByTheWay;
