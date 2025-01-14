b22a41cc46e2377eef315f659000d4c9be8c83be
dune <nathydune@gmail.com>
2025-01-14 12:36:54 +0100

    update cv

--- src/components/Curriculum/DownloadCv/DownloadCv.tsx
+++ src/components/Curriculum/DownloadCv/DownloadCv.tsx
@@ -6,7 +6,7 @@ import Styles from './DownloadCv.module.scss';
 const DownloadCv: React.FC = () => {
     return (
         <div className={Styles.container}>
-            <a href="/Cv/cv.pdf" download="Mon_CV.pdf"><Button type='download' text="TELECHARGER CV" /></a>
+            <a href="/Cv/cv.pdf" download="CV_Dune_Nathalie.pdf"><Button type='download' text="TELECHARGER CV" /></a>
         </div>
     )
 };
