74417bb7f5b1fe9033c024c981677de87581b291
dune <nathydune@gmail.com>
2025-01-17 19:17:41 +0100

    gestion zoom

--- src/Data/DataIllustrator.js
+++ src/Data/DataIllustrator.js
@@ -1,18 +1,31 @@
 import nath from '../assets/AI/nath.png';
-import idris from '../assets/AI/idris.png';
-import daryna from '../assets/AI/daryna.png';
+import fils from '../assets/AI/fils.png';
+import fille from '../assets/AI/fille.png';
 import renne from '../assets/AI/renne.png';
+import picsou from '../assets/AI/picsou.png';
+import sapinFils from '../assets/AI/sapin_fils.png';
+import sapinFille from '../assets/AI/sapin_fille.png';
+import boules from '../assets/AI/boules-noel.png';
+import totoro from '../assets/AI/totoro.png';
+import gifts from '../assets/AI/gifts.png';
+//import cardDaryna from '../assets/AI/card_fille.png';
+//import cardIdris from '../assets/AI/card_fils.png';
 
 
 const Data_illustrator = [
-    { id: 3, title:'Moi', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: nath},
-    { id: 1, title:'Idris', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: idris},
-    { id: 2, title:'Daryna', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: daryna},
-    { id: 4, title:'Renne', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: renne},
-    { id: 3, title:'Moi', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: nath},
-    { id: 1, title:'Idris', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: idris},
-    { id: 2, title:'Daryna', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: daryna},
-    { id: 4, title:'Renne', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: renne}
+    { id: 1, title:'Moi', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: nath},
+    { id: 2, title:'Fils', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: fils},
+    { id: 3, title:'Fille', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: fille},
+    { id: 4, title:'Picsou', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: picsou},
+    { id: 5, title:'Totoro', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: totoro},
+    { id: 6, title:'Renne', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: renne},
+    { id: 7, title:'Sapin', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: sapinFils},
+    { id: 8, title:'Sapin', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: sapinFille},
+    { id: 9, title:'Boules de noël', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: boules},
+    { id: 10, title:'Cadeaux', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: gifts}
+  /* { id: 11, title:'Carte de noel de ma fille', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: cardDaryna},
+    { id: 12, title:'Carte de noel de mon Fils', description:'Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.', img: cardIdris}
+*/
 ];
 
 export default Data_illustrator;
--- src/components/Illustrator/Illustrator.tsx
+++ src/components/Illustrator/Illustrator.tsx
@@ -5,23 +5,113 @@ import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
 const Illustrator: React.FC = () => {
-    return(
+    // Récupérer les données
+    const familyData = Data_illustrator.slice(0, 3);
+    const characterData = Data_illustrator.slice(3, 6);
+    const elementData = Data_illustrator.slice(6, 9);
+
+    return (
         <div className={Styles.container}>
-            {Data_illustrator.map((item, index) => (
-                <div key={index} className={Styles.item}>
-                    <Zoom>
-                        <div className={Styles.img}>
-                            <img src={item.img} alt={item.img} />
-                        </div>
-                    </Zoom>
-                    <div className={Styles.text}>
-                        <h1>{item.title}</h1>
-                        <p>{item.description}</p>
-                    </div>
+            {/* Section La Famille */}
+            <div className={Styles.category}>
+                <div className={Styles.title}>
+                    <h1>La famille:</h1>
                 </div>
-            ))}
+                <div className={Styles.images}>
+                    {familyData.map((item, index) => (
+                        <div key={index} className={Styles.item}>
+                           <Zoom
+                                zoomMargin={50}
+                                ZoomContent={({ img, style }) => {
+                                    const customStyle = {
+                                        ...style,
+                                        backgroundColor: 'rgba(196, 139, 122, 0.5)',
+                                        width: '100%',
+                                        height: '100%',
+                                    };
+                                    return <div style={customStyle}>{img}</div>;
+                                }}
+                            >
+                                <div className={Styles.img}>
+                                    <img src={item.img} alt={item.title} />
+                                </div>
+                            </Zoom>
+                            <div className={Styles.text}>
+                                <h1>{item.title}</h1>
+                                <p>{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* Section Les Personnages */}
+            <div className={Styles.category}>
+                <div className={Styles.title}>
+                    <h1>Les personnages:</h1>
+                </div>
+                <div className={Styles.images}>
+                    {characterData.map((item, index) => (
+                        <div key={index} className={Styles.item}>
+                            <Zoom
+                                zoomMargin={50}
+                                ZoomContent={({ img, style }) => {
+                                    const customStyle = {
+                                        ...style,
+                                        backgroundColor: 'rgba(196, 139, 122, 0.5)',
+                                        width: '100%',
+                                        height: '100%',
+                                    };
+                                    return <div style={customStyle}>{img}</div>;
+                                }}
+                            >
+                                <div className={Styles.img}>
+                                    <img src={item.img} alt={item.title} />
+                                </div>
+                            </Zoom>
+                            <div className={Styles.text}>
+                                <h1>{item.title}</h1>
+                                <p>{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* Section Les Éléments de Noël */}
+            <div className={Styles.category}>
+                <div className={Styles.title}>
+                    <h1>Les éléments de Noël:</h1>
+                </div>
+                <div className={Styles.images}>
+                    {elementData.map((item, index) => (
+                        <div key={index} className={Styles.item}>
+                            <Zoom
+                                zoomMargin={50}
+                                ZoomContent={({ img, style }) => {
+                                    const customStyle = {
+                                        ...style,
+                                        backgroundColor: 'rgba(196, 139, 122, 0.5)',
+                                        width: '100%',
+                                        height: '100%',
+                                    };
+                                    return <div style={customStyle}>{img}</div>;
+                                }}
+                            >
+                                <div className={Styles.img}>
+                                    <img src={item.img} alt={item.title} />
+                                </div>
+                            </Zoom>
+                            <div className={Styles.text}>
+                                <h1>{item.title}</h1>
+                                <p>{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Illustrator;
