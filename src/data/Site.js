cfe3a11786edf67703a78296f228fe36c61fbe5d
dune <nathydune@gmail.com>
2025-02-01 17:38:31 +0100

    update site filmagogo

--- src/data/Site.js
+++ src/data/Site.js
@@ -11,11 +11,9 @@ import imgAstro from '../../src/assets/Site/Astro.png';
 
 const Data_Sites = [
     {
-        id: 1, title: 'FilmaGogo', langage: "React, TypeScrypt, Javascript, SCSS,HTML, Nodes.js, Express.", description: `Filmagogo est un projet en cours de développement, inspiré d'une application bien connue permettant de consulter des films et séries. L'objectif est d'offrir une expérience utilisateur similaire en s'appuyant sur l'API de The Movie Database pour afficher les affiches, titres, notes et descriptions des œuvres.
+        id: 1, title: 'FilmaGogo', langage: "React, TypeScrypt, Javascript, SCSS,HTML, Nodes.js.", description: `Filmagogo est inspiré d'une application bien connue permettant de consulter des films et séries. L'objectif est d'offrir une expérience utilisateur similaire en s'appuyant sur l'API de The Movie Database pour afficher les affiches, titres, notes et descriptions des œuvres.
 
-La diffusion des films et séries n'est pas une fonctionnalité disponible. Cependant, pour certains d'entre eux, un lien permet d’être redirigé vers des sites ou plateformes où ils sont accessibles.
-
-Le projet est disponible sur mon GitHub, accessible via le lien ci-dessous.`, url: "https://github.com/DuneNathalie/filmagogo", icon: fag, img: imgFag
+La diffusion des films et séries n'est pas une fonctionnalité disponible. Cependant, pour certains d'entre eux, un lien permet d’être redirigé vers des sites ou plateformes où ils sont accessibles.`, url: "https://filmagogo.vercel.app/", icon: fag, img: imgFag
     },
     {
         id: 2, title: 'BazarDeFred', langage: "React, TypeScrypt, Javascript, HTML, SCSS, Nodes.js.", description: `Il s'agit d'un site vitrine que j'ai conçu pour ma sœur afin de mettre en valeur ses tableaux et son univers artistique. Ce site, simple et épuré, a été réalisé dans un délai court pour répondre rapidement à son besoin.
